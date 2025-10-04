3857d940a9de3f0f64e9635849138c40451c0c25
abhishekY2401 <abhiyd2401@gmail.com>
2025-10-04 13:18:04 +0530

    add comments for TC and SC

--- arrays/StockBuyAndSell.java
+++ arrays/StockBuyAndSell.java
@@ -1,12 +1,25 @@
 public class StockBuyAndSell {
+
+    /*
+     * Time Complexity - O(N)
+     * N is the number of elements in the array
+     * 
+     * Space Complexity - O(1)
+     * As no extra space is being used
+     * 
+     */
     public static int maxProfit(int[] prices) {
         int n = prices.length;
         int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
         for (int i = 0; i < n - 1; i++) {
+            // while we consider each stock, pick the min stock price.
             if (prices[i] < min) {
                 min = prices[i];
             }
+
+            // calculate the max profit after buying the lower stock price and selling on
+            // higher price
             max = Math.max(max, prices[i + 1] - min);
         }
 
