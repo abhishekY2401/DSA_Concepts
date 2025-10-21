8d3f738ce572ee3c9a4864afc2fa486cfb7497e8
abhishekY2401 <abhiyd2401@gmail.com>
2025-10-21 09:45:12 +0530

    three sum time and space complexity

--- arrays/ThreeSum.java
+++ arrays/ThreeSum.java
@@ -5,6 +5,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/*
+ * Time Complexity: O(N^2 * log(no. of unique triplets))
+ * 
+ * O(N^2) -> traverse with each element to find the triplet which sum to 0
+ * O(log triplets) -> inserting elements into the set
+ * 
+ * 
+ * Space Complexity: O(2 * no. of the unique triplets) + O(N)
+ * 
+ * O(2 * Triplet) - for unique triplets in the set and the answer list created at the end
+ * O(N) -> hashset being created for every ith number
+ */
 public class ThreeSum {
     public static List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> st = new HashSet<>();
@@ -14,6 +26,7 @@ public class ThreeSum {
             // store each jth element to find element k which gives difference of 0
             Set<Integer> hashset = new HashSet<>();
             for (int j = i + 1; j < nums.length; j++) {
+                // to make the triplet zero find the negation of both numbers
                 int thirdSum = -(nums[i] + nums[j]);
 
                 if (hashset.contains(thirdSum)) {
